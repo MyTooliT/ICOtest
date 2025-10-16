1dc2a6c163c874627ccb36b240c3e81e644f5497
René Schwaiger <sanssecours@me.com>
2025-10-16 11:18:56 +0200

    STU: Add test for firmware version

--- icotest/test/test_stu.py
+++ icotest/test/test_stu.py
@@ -58,3 +58,20 @@ async def test_eeprom_hardware_version(stu: STU):
         f"Written hardware version “{hardware_version_written}” does not"
         f"match read hardware version “{hardware_version_read}”"
     )
+
+
+async def test_eeprom_firmware_version(stu: STU):
+    """Test if reading and writing the firmware version works"""
+
+    # I am not sure, if the firmware already inits the EEPROM with the firmware
+    # version. Writing back the same firmware version into the EEPROM should
+    # not be a problem though.
+    firmware_version_written = await stu.get_firmware_version()
+    await stu.eeprom.write_firmware_version(firmware_version_written)
+    firmware_version_read = await stu.eeprom.read_firmware_version()
+    assert firmware_version_written == firmware_version_read, (
+        (
+            f"Written firmware version “{firmware_version_written}” does not "
+            f"match read firmware version “{firmware_version_read}”"
+        ),
+    )
