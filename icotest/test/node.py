55f41a7a21f7477d47702108e7e6b8be4cbfabda
René Schwaiger <sanssecours@me.com>
2025-10-16 15:13:42 +0200

    Test: Add general coroutine to check EEPROM values

--- icotest/test/node.py
+++ icotest/test/node.py
@@ -42,6 +42,34 @@ def assert_equal_read_write(
     ), f"Written {name} “{written}” does not match read {name} “{read}”"
 
 
+async def check_write_read(
+    node: SensorNode | STU, name: str, written: EEPROMValue
+) -> None:
+    """Check that a written and read EEPROM value match
+
+    Args:
+
+        node:
+                The node that should be checked
+
+        name:
+
+                The name of the EEPROM value
+
+        written:
+
+                The value that should be written and then read afterwards
+
+    """
+
+    function_name = name.lower().replace(" ", "_")
+    write_coroutine = getattr(node.eeprom, f"write_{function_name}")
+    read_coroutine = getattr(node.eeprom, f"read_{function_name}")
+    await write_coroutine(written)
+    read = await read_coroutine()
+    assert_equal_read_write(written, read, name)
+
+
 async def check_eeprom_gtin(node: SensorNode | STU, settings: DynaBox):
     """Test if reading and writing the GTIN works
 
@@ -56,10 +84,7 @@ async def check_eeprom_gtin(node: SensorNode | STU, settings: DynaBox):
 
     """
 
-    gtin_written = settings.gtin
-    await node.eeprom.write_gtin(gtin_written)
-    gtin_read = await node.eeprom.read_gtin()
-    assert_equal_read_write(gtin_written, gtin_read, "GTIN")
+    await check_write_read(node, "GTIN", settings.gtin)
 
 
 async def check_eeprom_hardware_version(
@@ -78,11 +103,8 @@ async def check_eeprom_hardware_version(
 
     """
 
-    hardware_version_written = Version.coerce(settings.hardware_version)
-    await node.eeprom.write_hardware_version(hardware_version_written)
-    hardware_version_read = await node.eeprom.read_hardware_version()
-    assert_equal_read_write(
-        hardware_version_written, hardware_version_read, "hardware version"
+    await check_write_read(
+        node, "hardware version", Version.coerce(settings.hardware_version)
     )
 
 
@@ -99,11 +121,8 @@ async def check_eeprom_firmware_version(node: SensorNode):
     # I am not sure, if the firmware already inits the EEPROM with the firmware
     # version. Writing back the same firmware version into the EEPROM should
     # not be a problem though.
-    firmware_version_written = await node.get_firmware_version()
-    await node.eeprom.write_firmware_version(firmware_version_written)
-    firmware_version_read = await node.eeprom.read_firmware_version()
-    assert_equal_read_write(
-        firmware_version_written, firmware_version_read, "firmware version"
+    await check_write_read(
+        node, "firmware version", await node.get_firmware_version()
     )
 
 
