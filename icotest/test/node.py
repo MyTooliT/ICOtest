2b9e1c917d9e24eea9d807d37da2f5ae87979283
René Schwaiger <sanssecours@me.com>
2025-10-16 16:34:48 +0200

    EEPROM: Add test for power off cycles

--- icotest/test/node.py
+++ icotest/test/node.py
@@ -96,3 +96,4 @@ async def check_eeprom_statistics(node: SensorNode):
     """
 
     await check_write_read_eeprom(node, "power on cycles", 0)
+    await check_write_read_eeprom(node, "power off cycles", 0)
