a30b1813f54fa13fc6c9c99a40f8b02bbefd5e4c
Norbert Leder <norbert.leder@mytoolit.com>
2026-03-18 15:39:48 +0100

    Remove deprecated --log argument from test runner

--- icotest/gui/workers/test_runner.py
+++ icotest/gui/workers/test_runner.py
@@ -71,8 +71,6 @@ class TestRunner(QThread):
             self.device_name,
             "--test-group",
             self.test_group,
-            "--log",
-            self.log_level,
         ]
 
         if self.backpack_model == "None":
