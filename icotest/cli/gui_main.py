5c69a92dd597a9ffb1a9e5ac6ccaf7ceaa880e7f
René Schwaiger <sanssecours@me.com>
2026-07-01 15:57:47 +0200

    Lint: Address pylint problems

--- icotest/cli/gui_main.py
+++ icotest/cli/gui_main.py
@@ -1,15 +1,21 @@
 """Entry point for the ICOtest graphical user interface"""
 
 import sys
-from PySide6.QtWidgets import QApplication
+
+
+from PySide6.QtWidgets import QApplication  # pylint: disable=no-name-in-module
 
 
 def main():
     """Start the ICOtest Production Assistant GUI"""
 
+    # pylint: disable=import-outside-toplevel
+
     # Imports inside main to avoid Qt loading if someone just imports cli
     # module
     from icotest.gui.main_window import MainWindow
+
+    # pylint: enable=import-outside-toplevel
 
     app = QApplication(sys.argv)
     app.setApplicationName("ICOtest Production Assistant")
